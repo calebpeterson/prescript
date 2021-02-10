3f7a1a108e0e8cfb4467654b215c1cfbc9563cc6
Caleb Peterson <caleb.peterson@zapier.com>
2021-02-10 14:12:57 -0500

    add additional executeIntoDocument and executeInBackground commands to the extension

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ No known issues at this time.
 ### 1.0.0
 
 Initial release of Prescript
+
+### 2.0.0
+
+Add the `Execute Into Document` command which will pipe the command output into an untitled document rather than a terminal.
+
+### 2.0.1
+
+Add the `Execute In Background` command which will execute the command w/o revealing the terminal.
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,18 @@
 import * as vscode from "vscode";
 import { dirname } from "path";
+import { exec } from "child_process";
 
 import getOrCreateTerminal from "./utils/getOrCreateTerminal";
 
 function getCommand(): string | undefined {
   const activeEditor = vscode.window.activeTextEditor;
   if (activeEditor) {
-    const selection = activeEditor.selection;
-    const line = activeEditor.document.lineAt(selection.active);
-    return line.text;
+    const { selection, document } = activeEditor;
+    if (selection.isEmpty) {
+      const line = document.lineAt(selection.active);
+      return line.text;
+    }
+    return document.getText(selection);
   }
 }
 
@@ -30,6 +34,34 @@ export function activate(context: vscode.ExtensionContext) {
       if (cmd && cwd) {
         const terminal = getOrCreateTerminal(cwd);
         terminal.sendText(cmd);
+      }
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("prescript.executeInBackground", () => {
+      const cmd = getCommand();
+      const cwd = getWorkingDirectory();
+
+      if (cmd && cwd) {
+        const terminal = getOrCreateTerminal(cwd, { show: false });
+        terminal.sendText(cmd);
+      }
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("prescript.executeIntoDocument", () => {
+      const cmd = getCommand();
+      const cwd = getWorkingDirectory();
+
+      if (cmd && cwd) {
+        exec(cmd, { cwd }, async (err, stdout, stderr) => {
+          const body = err ? err.message : stdout || stderr;
+          const content = `${cmd}\n\n${body}`;
+          const outputDocument = await vscode.workspace.openTextDocument({
+            content,
+          });
+          vscode.window.showTextDocument(outputDocument);
+        });
       }
     })
   );
